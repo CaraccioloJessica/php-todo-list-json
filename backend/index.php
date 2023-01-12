7c76c4c4e3810269187bfda7ed3297b6526cba6e
Jessica <jesscara.993@gmail.com>
2023-01-12 16:50:24 +0100

    array list

---
+++ backend/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$todoList = [
+  [
+    "text" => "HTML",
+    "completed" => true
+  ],
+  [
+    "text" => "CSS",
+    "completed" => true
+  ],
+  [
+    "text" => "Responsive design",
+    "completed" => true
+  ],
+  [
+    "text" => "Javascript",
+    "completed" => true
+  ],
+  [
+    "text" => "PHP",
+    "completed" => false
+  ],
+  [
+    "text" => "Laravel",
+    "completed" => false
+  ]
+];
+
+var_dump($todoList);
