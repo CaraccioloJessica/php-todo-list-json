4153d778e84afb5a5362bad2ffe828349bfd090f
Jessica <jesscara.993@gmail.com>
2023-01-12 17:35:24 +0100

    axios + moved template from component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,89 @@
 <script>
-import TemplateTodo from './components/TemplateTodo.vue'
+import axios from 'axios';
+
+const API_URL = "http://localhost/";
 
 export default {
-  components: {
-    TemplateTodo,
+  data() {
+    return {
+      newTodo: "",
+      todoList: [],
+    };
+  },
+
+  methods: {
+    formSubmit(e) {
+      e.preventDefault();
+
+      const params = {
+        params: {
+          'newTodo': this.newTodo
+        }
+      };
+
+      axios.get(API_URL + "api-create-todo.php", params)
+        .then(() => {
+          this.getAllData();
+        });
+    },
+    getAllData() {
+      axios.get(API_URL + "api.php")
+        .then(res => {
+          const data = res.data;
+          this.todoList = data;
+        });
+    }
+  },
+  mounted() {
+    this.getAllData();
   }
 }
 </script>
 
 <template>
-  <TemplateTodo />
+  <div class="container">
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="(todoElem, ind) in todoList" :key="ind" :class="{ 'completed': todoElem.completed }">
+        <a href="#"> {{ todoElem.text }} </a>
+      </li>
+    </ul>
+
+    <form @submit="formSubmit">
+      <input type="text" name="newTodo" v-model="newTodo">
+      <input type="submit" value="Inserisci">
+    </form>
+  </div>
 </template>
 
 <style>
+h1 {
+  margin: 20px 0 30px;
+}
 
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+li {
+  display: inline-block;
+  margin: 6px 0;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+form {
+  margin-top: 20px;
+}
+
+.completed {
+  text-decoration: line-through;
+}
 </style>
